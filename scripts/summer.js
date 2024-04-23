15ed3dcfe7aacfe4046eec2f92f0e0828bbc313e
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2024-04-23 13:03:23 +0600

    added

---
+++ scripts/summer.js
@@ -0,0 +1,15 @@
+// const cards1 =document.querySelectorAll('.cards');
+// // console.log(cards1);
+
+// for(const i=0; i<cards1.length; i++){
+//     const card = cards1[i];
+//     // console.log(element);
+//     card.addEventListener('click',function(){
+//         console.log('clicked');
+//     })
+// }
+
+
+
+
+
